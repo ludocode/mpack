2f10a035ffb6d538b56ee188effd6bb81b51e86c
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2015-04-16 12:05:47 -0400

    Fixed cleanup of coverage gcno files

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scons -c
-rm -f .sconsign.dblite mpack-*.tar.gz *.gcov src/mpack/*.o README.html
+rm -f .sconsign.dblite mpack-*.tar.gz *.gcov *.gcno src/mpack/*.o README.html
 rm -f projects/vs/*.{suo,sdf,opensdf}
 rm -rf build docs
 rm -rf projects/vs/{Debug,Release}
