3c7e022c882d909811059c34df3bd0076522487f
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2015-07-12 16:29:29 -0400

    Made mpack writer growable buffer intrusive to avoid buffer copies

--- src/mpack/mpack-writer.c
+++ src/mpack/mpack-writer.c
@@ -54,59 +54,80 @@ void mpack_writer_init_error(mpack_writer_t* writer, mpack_error_t error) {
 
 #ifdef MPACK_MALLOC
 typedef struct mpack_growable_writer_t {
-    char* data;
-    size_t size;
-    size_t capacity;
-
     char** target_data;
     size_t* target_size;
-
-    char buffer[MPACK_BUFFER_SIZE];
 } mpack_growable_writer_t;
 
-static void mpack_growable_writer_flush(mpack_writer_t* writer, const char* buffer, size_t count) {
-    mpack_growable_writer_t* growable_writer = (mpack_growable_writer_t*)writer->context;
+static void mpack_growable_writer_flush(mpack_writer_t* writer, const char* data, size_t count) {
 
-    if (growable_writer->size + count > growable_writer->capacity) {
-        size_t new_capacity = growable_writer->size + count;
-        if (new_capacity < growable_writer->capacity * 2)
-            new_capacity = growable_writer->capacity * 2;
+    // This is an intrusive flush function which modifies the writer's buffer
+    // in response to a flush instead of emptying it in order to add more
+    // capacity for data. This removes the need to copy data from a fixed buffer
+    // into a growable one, improving performance.
+    //
+    // There are three ways flush can be called:
+    //   - flushing the buffer during writing (used is zero, count is all data, data is buffer)
+    //   - flushing extra data during writing (used is all flushed data, count is extra data, data is not buffer)
+    //   - flushing during teardown (used and count are both all flushed data, data is buffer)
+    //
+    // We handle these here, making sure used is the total count in all three cases.
+    mpack_log("flush size %i used %i data %p buffer %p\n", (int)writer->size, (int)writer->used, data, writer->buffer);
 
-        char* new_data = (char*)MPACK_MALLOC(new_capacity);
-        if (new_data == NULL) {
+    // if the given data is not the old buffer, we'll need to actually copy it into the buffer
+    bool is_extra_data = (data != writer->buffer);
+
+    // if we're flushing all data (used is zero), we should actually grow
+    size_t new_size = writer->size;
+    if (writer->used == 0 && count != 0)
+        new_size *= 2;
+    while (new_size < (is_extra_data ? writer->used + count : count))
+        new_size *= 2;
+
+    if (new_size > writer->size) {
+        mpack_log("flush growing from %i to %i\n", (int)writer->size, (int)new_size);
+        char* new_buffer = (char*)MPACK_MALLOC(new_size);
+        if (new_buffer == NULL) {
             mpack_writer_flag_error(writer, mpack_error_memory);
             return;
         }
-
-        if (growable_writer->data) {
-            mpack_memcpy(new_data, growable_writer->data, growable_writer->size);
-            MPACK_FREE(growable_writer->data);
-        }
-        growable_writer->data = new_data;
-        growable_writer->capacity = new_capacity;
+        mpack_memcpy(new_buffer, writer->buffer, count);
+        MPACK_FREE(writer->buffer);
+        writer->buffer = new_buffer;
+        writer->size = new_size;
     }
 
-    mpack_memcpy(growable_writer->data + growable_writer->size, buffer, count);
-    growable_writer->size += count;
+    if (is_extra_data) {
+        mpack_memcpy(writer->buffer + writer->used, data, count);
+        // add our extra data to count
+        writer->used += count;
+    } else {
+        // used is either zero or count; set it to count
+        writer->used = count;
+    }
 }
 
 static void mpack_growable_writer_teardown(mpack_writer_t* writer) {
     mpack_growable_writer_t* growable_writer = (mpack_growable_writer_t*)writer->context;
 
     if (mpack_writer_error(writer) == mpack_ok) {
-        *growable_writer->target_data = growable_writer->data;
-        *growable_writer->target_size = growable_writer->size;
+        // TODO: realloc this to fit in case the buffer is much
+        // larger than the final data size
+        *growable_writer->target_data = writer->buffer;
+        *growable_writer->target_size = writer->used;
+        writer->buffer = NULL;
 
-    } else if (growable_writer->data) {
-        MPACK_FREE(growable_writer->data);
+    } else if (writer->buffer) {
+        MPACK_FREE(writer->buffer);
+        writer->buffer = NULL;
     }
 
     MPACK_FREE(growable_writer);
+    writer->context = NULL;
 }
 
-void mpack_writer_init_growable(mpack_writer_t* writer, char** data, size_t* size) {
-    *data = NULL;
-    *size = 0;
+void mpack_writer_init_growable(mpack_writer_t* writer, char** target_data, size_t* target_size) {
+    *target_data = NULL;
+    *target_size = 0;
 
     mpack_growable_writer_t* growable_writer = (mpack_growable_writer_t*) MPACK_MALLOC(sizeof(mpack_growable_writer_t));
     if (growable_writer == NULL) {
@@ -115,10 +136,13 @@ void mpack_writer_init_growable(mpack_writer_t* writer, char** data, size_t* siz
     }
     mpack_memset(growable_writer, 0, sizeof(*growable_writer));
 
-    growable_writer->target_data = data;
-    growable_writer->target_size = size;
+    growable_writer->target_data = target_data;
+    growable_writer->target_size = target_size;
 
-    mpack_writer_init(writer, growable_writer->buffer, sizeof(growable_writer->buffer));
+    size_t capacity = MPACK_BUFFER_SIZE;
+    char* buffer = (char*)MPACK_MALLOC(capacity);
+
+    mpack_writer_init(writer, buffer, capacity);
     mpack_writer_set_context(writer, growable_writer);
     mpack_writer_set_flush(writer, mpack_growable_writer_flush);
     mpack_writer_set_teardown(writer, mpack_growable_writer_teardown);
@@ -194,9 +218,15 @@ static void mpack_write_native_big(mpack_writer_t* writer, const char* p, size_t
             "space in buffer. call mpack_write_native() instead",
             (int)count, (int)(writer->size - writer->used));
 
+    // we'll need a flush function
+    if (!writer->flush) {
+        mpack_writer_flag_error(writer, mpack_error_io);
+        return;
+    }
+
     // we assume that the flush function is orders of magnitude slower
-    // than memcpy(), so we always fill the buffer as much as possible
-    // and write only in multiples of the buffer size.
+    // than memcpy(), so we fill the buffer up first to try to flush as
+    // infrequently as possible.
     
     // fill the remaining space in the buffer
     size_t n = writer->size - writer->used;
@@ -209,31 +239,26 @@ static void mpack_write_native_big(mpack_writer_t* writer, const char* p, size_t
     if (count == 0)
         return;
 
-    // we'll need a flush function
-    if (!writer->flush) {
-        mpack_writer_flag_error(writer, mpack_error_io);
-        return;
-    }
-
     // flush the buffer
-    writer->flush(writer, writer->buffer, writer->used);
+    size_t used = writer->used;
+    writer->used = 0;
+    writer->flush(writer, writer->buffer, used);
     if (mpack_writer_error(writer) != mpack_ok)
         return;
-    writer->used = 0;
 
-    // flush any data that doesn't fit in the buffer
-    n = count - (count % writer->size);
-    if (n > 0) {
-        writer->flush(writer, p, n);
+    // note that an intrusive flush function (such as mpack_growable_writer_flush())
+    // may have changed size and/or reset used to a non-zero value. we treat both as
+    // though they may have changed, and there may still be data in the buffer.
+
+    // flush the extra data directly if it doesn't fit in the buffer
+    if (count > writer->size - writer->used) {
+        writer->flush(writer, p, count);
         if (mpack_writer_error(writer) != mpack_ok)
             return;
-        p += n;
-        count -= n;
+    } else {
+        mpack_memcpy(writer->buffer + writer->used, p, count);
+        writer->used += count;
     }
-
-    // copy any remaining data into the buffer
-    mpack_memcpy(writer->buffer, p, count);
-    writer->used += count;
 }
 
 static inline void mpack_write_native(mpack_writer_t* writer, const char* p, size_t count) {
@@ -352,12 +377,13 @@ mpack_error_t mpack_writer_destroy(mpack_writer_t* writer) {
     // flush any outstanding data
     if (mpack_writer_error(writer) == mpack_ok && writer->used != 0 && writer->flush != NULL) {
         writer->flush(writer, writer->buffer, writer->used);
-        writer->used = 0;
+        writer->flush = NULL;
     }
 
-    if (writer->teardown)
+    if (writer->teardown) {
         writer->teardown(writer);
-    writer->teardown = NULL;
+        writer->teardown = NULL;
+    }
 
     #if MPACK_SETJMP
     if (writer->jump_env)
