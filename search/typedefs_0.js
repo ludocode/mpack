6e00ff4ad1d6e0eae1b7b8e5db7887b6958ddda0
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2015-09-27 11:58:07 -0400

    Updated documentation to version 0.6

--- search/typedefs_0.js
+++ search/typedefs_0.js
@@ -1,11 +1,14 @@
 var searchData=
 [
-  ['mpack_5ffill_5ft',['mpack_fill_t',['../group__reader.html#gaf0569b7903313adf8cf5d112101e7d62',1,'mpack-reader.h']]],
-  ['mpack_5fflush_5ft',['mpack_flush_t',['../group__writer.html#gaf1a55f896e971b563506c45f4195920e',1,'mpack-writer.h']]],
   ['mpack_5fnode_5fdata_5ft',['mpack_node_data_t',['../group__node.html#ga37218aec593259aed8b28625ff1395fb',1,'mpack-node.h']]],
   ['mpack_5fnode_5ft',['mpack_node_t',['../group__node.html#ga08f46371c0c59a2e4dd7bdf1891fb3cb',1,'mpack-node.h']]],
+  ['mpack_5freader_5ferror_5ft',['mpack_reader_error_t',['../group__reader.html#ga799a3f21adc8e229c6040ab42d90bef3',1,'mpack-reader.h']]],
+  ['mpack_5freader_5ffill_5ft',['mpack_reader_fill_t',['../group__reader.html#ga97503ca06d41d58c60226b2732661eda',1,'mpack-reader.h']]],
   ['mpack_5freader_5fteardown_5ft',['mpack_reader_teardown_t',['../group__reader.html#gadda6baaccdcf0ad9c5e212be9992cf5b',1,'mpack-reader.h']]],
+  ['mpack_5ftree_5ferror_5ft',['mpack_tree_error_t',['../group__node.html#ga184ff3fbec66290ec72caf0abc8f3594',1,'mpack-node.h']]],
   ['mpack_5ftree_5ft',['mpack_tree_t',['../group__node.html#ga9c76cfdb08042d6412b5c745bca928a7',1,'mpack-node.h']]],
   ['mpack_5ftree_5fteardown_5ft',['mpack_tree_teardown_t',['../group__node.html#ga1ca1eb6735624e7d98d64ac77b525c55',1,'mpack-node.h']]],
+  ['mpack_5fwriter_5ferror_5ft',['mpack_writer_error_t',['../group__writer.html#ga65d410be0305a59ec15cfe700f8b036a',1,'mpack-writer.h']]],
+  ['mpack_5fwriter_5fflush_5ft',['mpack_writer_flush_t',['../group__writer.html#gad5749babfacaa707cbaff06bcbc790a5',1,'mpack-writer.h']]],
   ['mpack_5fwriter_5fteardown_5ft',['mpack_writer_teardown_t',['../group__writer.html#ga97ccab59b332d882d386c10b241eb888',1,'mpack-writer.h']]]
 ];
