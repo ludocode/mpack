f9476ab323fc330ab215b1be4586b28d147e4865
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2016-03-20 00:17:09 -0400

    Removed merge-badges.sh

--- tools/merge-badges.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# A wrapper around git merge that updates the
-# branch for README.md badges
-[[ -z $(git status --porcelain) ]] || { git status --porcelain; echo "Tree is not clean!" ; exit 1; }
-git merge --no-ff --no-edit $1
-branch=`git rev-parse --abbrev-ref HEAD`
-sed "s@branch\([=/]\)$1@branch\1$branch@g" -i README.md
-git add README.md
-git commit --amend --no-edit
