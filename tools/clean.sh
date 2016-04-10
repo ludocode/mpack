528e72d76c245c234dcb5f17c7986b7ae56c16ae
Nicholas Fraser <nicholas.rd.fraser@gmail.com>
2016-04-10 13:25:27 -0400

    Cleaned up vgcore files left by Valgrind

--- tools/clean.sh
+++ tools/clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 scons -c
-rm -rf .sconsign.dblite .sconf_temp config.log
+rm -rf .sconsign.dblite .sconf_temp config.log vgcore.*
 rm -f mpack-*.tar.gz *.gcov *.gcno src/mpack/*.o README.html
 rm -f projects/vs/*.{suo,sdf,opensdf}
 rm -rf build analysis
