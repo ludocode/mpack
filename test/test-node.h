618a376fcbe0e566a3a333e60652951dbcb31984
Nicholas Fraser <nick@ludocode.com>
2018-10-12 23:57:17 -0400

    Updated copyright dates

--- test/test-node.h
+++ test/test-node.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015-2016 Nicholas Fraser
+ * Copyright (c) 2015-2018 Nicholas Fraser
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
