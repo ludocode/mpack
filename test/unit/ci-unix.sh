39aed08f2b253a2a9ae122f18c42b4befee4c302
Nicholas Fraser <nick@ludocode.com>
2020-09-12 00:41:45 -0400

    Add new Python buildsystem with MSVC support

---
+++ test/unit/ci-unix.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+# Builds and runs the unit test suite under UNIX.
+#
+# Pass environment variable CC to specify the compiler.
+#
+# This script is run by the continuous integration server to test MPack on UNIX
+# systems.
+
+set -e
+
+if [ "$AMALGAMATED" = "1" ]; then
+    tools/amalgamate.sh
+    cd build/amalgamation
+fi
+pwd
+
+if [[ "$CC" == "scan-build" ]]; then
+    unset CC
+    unset CXX
+
+    echo "Not yet implemented!"
+    exit 1
+
+    scan-build -o analysis --use-cc=`which clang` --status-bugs test/unit/configure.py
+    ninja -f build/unit/build.ninja
+    exit $?
+fi
+
+if [ "$CC" = "gcov" ]; then
+    unset CC
+    unset CXX
+    test/unit/configure.py
+    ninja -f build/unit/build.ninja run-coverage
+    tools/gcov.sh
+    pip install --user idna==2.5 # below packages conflict with idna-2.6 (not sure if this is still necessary on bionic)
+    pip install --user cpp-coveralls urllib3[secure]
+    coveralls --no-gcov --include src
+    exit $?
+fi
+
+test/unit/configure.py
+ninja -f build/unit/build.ninja all
