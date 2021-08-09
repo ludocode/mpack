d200d2106de9e3f1bbf0ba2213f60ea15d3658d9
Nicholas Fraser <nick@ludocode.com>
2021-08-08 23:56:59 -0400

    Fix coverage script

--- tools/coverage.sh
+++ tools/coverage.sh
@@ -7,15 +7,15 @@ tools/unit.sh run-coverage
 # run gcov for traditional text-based coverage output
 rm -rf coverage
 mkdir -p coverage
-gcov --object-directory test/.build/coverage/objs/src/mpack `find src -name '*.c'` || exit $?
+gcov --object-directory .build/unit/coverage/objs/src/mpack `find src -name '*.c'` || exit $?
 mv *.gcov coverage
 
 # run lcov
-lcov --capture --directory test/.build/coverage/objs/src --output-file coverage/lcov.info
+lcov --capture --directory .build/unit/coverage/objs/src --output-file coverage/lcov.info
 
 # generate HTML coverage
 genhtml coverage/lcov.info --output-directory coverage/html
 
 echo
 echo "Done. Results written in coverage/"
-echo "View HTML results in: file://$(pwd)/coverage/index.html"
+echo "View HTML results in: file://$(pwd)/coverage/html/index.html"
