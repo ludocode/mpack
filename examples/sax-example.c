91df0e8a75af2649c2d5192bfd801b80d8d26981
Nikita Danilov <danilov@gmail.com>
2022-12-16 10:36:24 -0800

    examples/sax-example.c: add bin support, indent output.
    Signed-off-by: Nikita Danilov <danilov@gmail.com>

--- examples/sax-example.c
+++ examples/sax-example.c
@@ -26,49 +26,57 @@ static void parse_element(mpack_reader_t* reader, int depth,
 
     switch (mpack_tag_type(&tag)) {
         case mpack_type_nil:
-            callbacks->nil_element(context);
+            callbacks->nil_element(context, depth);
             break;
         case mpack_type_bool:
-            callbacks->bool_element(context, mpack_tag_bool_value(&tag));
+            callbacks->bool_element(context, depth, mpack_tag_bool_value(&tag));
             break;
         case mpack_type_int:
-            callbacks->int_element(context, mpack_tag_int_value(&tag));
+            callbacks->int_element(context, depth, mpack_tag_int_value(&tag));
             break;
         case mpack_type_uint:
-            callbacks->uint_element(context, mpack_tag_uint_value(&tag));
+            callbacks->uint_element(context, depth, mpack_tag_uint_value(&tag));
             break;
 
         case mpack_type_str: {
             uint32_t length = mpack_tag_str_length(&tag);
             const char* data = mpack_read_bytes_inplace(reader, length);
-            callbacks->string_element(context, data, length);
+            callbacks->string_element(context, depth, data, length);
             mpack_done_str(reader);
+            break;
+        }
+
+        case mpack_type_bin: {
+            uint32_t length = mpack_tag_bin_length(&tag);
+            const char* data = mpack_read_bytes_inplace(reader, length);
+            callbacks->bin_element(context, depth, data, length);
+            mpack_done_bin(reader);
             break;
         }
 
         case mpack_type_array: {
             uint32_t count = mpack_tag_array_count(&tag);
-            callbacks->start_array(context, count);
+            callbacks->start_array(context, depth, count);
             while (count-- > 0) {
                 parse_element(reader, depth + 1, callbacks, context);
                 if (mpack_reader_error(reader) != mpack_ok) // critical check!
                     break;
             }
-            callbacks->finish_array(context);
+            callbacks->finish_array(context, depth);
             mpack_done_array(reader);
             break;
         }
 
         case mpack_type_map: {
             uint32_t count = mpack_tag_map_count(&tag);
-            callbacks->start_map(context, count);
+            callbacks->start_map(context, depth, count);
             while (count-- > 0) {
                 parse_element(reader, depth + 1, callbacks, context);
                 parse_element(reader, depth + 1, callbacks, context);
                 if (mpack_reader_error(reader) != mpack_ok) // critical check!
                     break;
             }
-            callbacks->finish_map(context);
+            callbacks->finish_map(context, depth);
             mpack_done_map(reader);
             break;
         }
@@ -88,41 +96,67 @@ static void parse_element(mpack_reader_t* reader, int depth,
 #include <unistd.h>
 #include <sys/mman.h>
 
-static void nil_element(void* context) {
+static void indent(int depth) {
+    const char ruler[] = "                                                   ";
+    printf("%*.*s", depth * 4, depth * 4, ruler);
+}
+
+static void nil_element(void* context, int depth) {
+    indent(depth);
     printf("nil\n");
 }
 
-static void bool_element(void* context, int64_t value) {
+static void bool_element(void* context, int depth, int64_t value) {
+    indent(depth);
     printf("bool: %s\n", value ? "true" : "false");
 }
 
-static void int_element(void* context, int64_t value) {
+static void int_element(void* context, int depth, int64_t value) {
+    indent(depth);
     printf("int: %" PRIi64 "\n", value);
 }
 
-static void uint_element(void* context, uint64_t value) {
+static void uint_element(void* context, int depth, uint64_t value) {
+    indent(depth);
     printf("uint: %" PRIu64 "\n", value);
 }
 
-static void string_element(void* context, const char* data, uint32_t length) {
+static void string_element(void* context, int depth,
+			   const char* data, uint32_t length) {
+    indent(depth);
     printf("string: \"");
     fwrite(data, 1, length, stdout);
     printf("\"\n");
 }
 
-static void start_map(void* context, uint32_t pair_count) {
+static void bin_element(void* context, int depth,
+			const char* data, uint32_t length) {
+    uint32_t i;
+    indent(depth);
+    printf("bin: \"");
+    for (i = 0; i < length; ++i) {
+	    printf("%02.2x", data[i] & 0xff);
+    }
+    printf("\"\n");
+}
+
+static void start_map(void* context, int depth, uint32_t pair_count) {
+    indent(depth);
     printf("starting map of %u key-value pairs\n", pair_count);
 }
 
-static void start_array(void* context, uint32_t element_count) {
+static void start_array(void* context, int depth, uint32_t element_count) {
+    indent(depth);
     printf("starting array of %u key-value pairs\n", element_count);
 }
 
-static void finish_map(void* context) {
+static void finish_map(void* context, int depth) {
+    indent(depth);
     printf("finishing map\n");
 }
 
-static void finish_array(void* context) {
+static void finish_array(void* context, int depth) {
+    indent(depth);
     printf("finishing array\n");
 }
 
@@ -132,11 +166,13 @@ static sax_callbacks_t callbacks = {
     int_element,
     uint_element,
     string_element,
+    bin_element,
     start_map,
     start_array,
     finish_map,
     finish_array,
 };
+
 
 int main(int argc, char** argv) {
     struct stat stat;
